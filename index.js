59184787ed697c80c6730e91c05b6cf7f06ad527
Pranav Mujumdar <psmujumdar@gmail.com>
2020-08-20 11:32:59 -0400

    Catching the stream in buffer using StringDecoder

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
  */
 'use strict'
  // Dependancies
- const http = require('http'); //for handling the http requests
- const url = require("url"); // for getting the url and it's path
-
- // 
+const http = require('http'); //for handling the http requests
+const url = require("url"); // for getting the url and it's path
+const { runInNewContext } = require('vm');
+const stringDecoder = require('string_decoder').StringDecoder;
+ 
+// Server config 
 const server = http.createServer((req, res) => {
     // get the url
     var parsedUrl = url.parse(req.url, true);
@@ -27,14 +29,26 @@ const server = http.createServer((req, res) => {
     // get the headers from the req
     var headers = req.headers;
 
-    // send the response
-    console.log("Hello");
-    
-    // log the request
-    console.log("request receieved on path "+ trimmedPath+" and with these query String params ",queryStringObject);
-    console.log("headers ", headers);
-    res.end(`request made for ${trimmedPath} with ${method}\n`);
+    // get the payloads from the request
+    var decoder = new stringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', (data)=> {
+        buffer += decoder.write(data);
+    })
+
+    req.on('end',()=>{
+        buffer += decoder.end();
+        // send the response
+        console.log("Hello");
+        
+        // log the request
+        console.log("request receieved on path "+ trimmedPath+" and with these query String params ",queryStringObject);
+        console.log("headers ", headers);
+        console.log("PAyload ", buffer);
+        res.end(`request made for ${trimmedPath} with ${method}\n`);
+    }) 
 })
+
  // start the server at port 3000
  server.listen(3000, ()=>{
      console.log("Server up on port 3000!");
