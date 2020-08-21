fbdacf2a196a72a87aa5660edb32f381fd010355
Pranav Mujumdar <psmujumdar@gmail.com>
2020-08-21 10:47:45 -0400

    Router handlers set up

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const http = require('http'); //for handling the http requests
 const url = require("url"); // for getting the url and it's path
 const { runInNewContext } = require('vm');
+const { type } = require('os');
 const stringDecoder = require('string_decoder').StringDecoder;
  
 // Server config 
@@ -38,14 +39,48 @@ const server = http.createServer((req, res) => {
 
     req.on('end',()=>{
         buffer += decoder.end();
-        // send the response
-        console.log("Hello");
+
+        //choose the handler, if not found go to not found
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        //construct the data object to the handler
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        } 
+
+        // route the request to the handler
+        chosenHandler(data, function(statusCode, payload){
+            // use status code called back by the handler of default to 200
+            statusCode = typeof(statusCode) == 'number'? statusCode : 200;
+            //use the payload by the handler or default empty 
+            payload = typeof(payload) == 'object'? payload : {};
+
+            // convert payload to string 
+            var payloadString = JSON.stringify(payload);
+
+            //return the response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            //log the response
+
+            console.log('Returning this string ', statusCode,payloadString);
+        });
+        // // send the response
+        // console.log("Hello");
         
-        // log the request
-        console.log("request receieved on path "+ trimmedPath+" and with these query String params ",queryStringObject);
-        console.log("headers ", headers);
-        console.log("PAyload ", buffer);
-        res.end(`request made for ${trimmedPath} with ${method}\n`);
+        // // log the request
+        // console.log("request receieved on path "+ trimmedPath+" and with these query String params ",queryStringObject);
+        // console.log("headers ", headers);
+        // console.log("PAyload ", buffer);
+        // res.end(`request made for ${trimmedPath} with ${method}\n`);
+
+
+
     }) 
 })
 
@@ -53,3 +88,24 @@ const server = http.createServer((req, res) => {
  server.listen(3000, ()=>{
      console.log("Server up on port 3000!");
  })
+// ahndlers
+
+var handlers = {};
+
+handlers.sample = (data, callback) =>
+{
+    // callback an http status code and a payload object
+    callback(406,{'name':'sample handler 406'});
+};
+
+handlers.notFound = (data, callback) =>
+{
+    callback(404);
+};
+
+//Routes
+
+
+var router = {
+    'sample' : handlers.sample 
+}
